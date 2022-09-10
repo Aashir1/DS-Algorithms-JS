a6aa836031188c6d21a58cd503c819b2bb19b395
Aashir Khan <kaashir9@gmail.com>
2022-09-10 16:39:12 +0500

    initial commit

---
+++ binarySearch.js
@@ -0,0 +1,19 @@
+//Divide and conquerer technique
+
+function searchAlgo(array, number) {
+    let min = 0, max = array.length - 1;
+
+    while (min <= max) {
+        let midIndex = Math.floor((min + max) / 2);
+        if (array[midIndex] < number) {
+            min = midIndex + 1;
+        } else if (array[midIndex] > number) {
+            max = midIndex - 1;
+        } else {
+            return midIndex;
+        }
+    }
+    return -1;
+}
+
+console.log(searchAlgo([1,2,3,4,5,6,7,8,9,10], 9));
